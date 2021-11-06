f1162aa16ad69363231ac27de2baace9512d61d9
gallifreyancat <galaxygirl418@icloud.com>
2021-11-06 16:43:20 -0500

    11-6 Meeting Updates

--- cargo_library.py
+++ cargo_library.py
@@ -21,8 +21,7 @@ left_sensor = ColorSensor(Port.S1)
 right_sensor = ColorSensor(Port.S4)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=79, axle_track=111)
 gyro = GyroSensor(Port.S2, Direction.COUNTERCLOCKWISE)
-#robot.distance_control.limits(400, 300, 100)
-#robot.heading_control.limits(300,100,100)
+timer = StopWatch()
 
 def beep():
     ev3.speaker.beep()
@@ -31,23 +30,16 @@ def reset(angle):
     robot.reset()
     gyro.reset_angle(angle)
 
-def turn(angle):
-    robot.turn(angle)
+# def turn(angle):
+#     robot.turn(angle)
 
 
 def gyro_drive(speed, distance, angle):
     robot.reset()
     drive_distance = robot.distance()
     kp = 5
-    kd = 1
     new_speed = speed
     while drive_distance < distance:
-        # if drive_distance >= distance * .75 and kd >= .4:
-        #     kd = kd - .2
-        #     new_speed = speed * kd
-            #print("kd =", kd)
-        # else:
-        #     print("No Change to Speed")
         deviation = angle - gyro.angle()
         turn_rate = kp * deviation
         robot.drive(new_speed, turn_rate)
@@ -58,8 +50,8 @@ def gyro_drive(speed, distance, angle):
     print("Angle =", gyro.angle(), "Should be", angle)
     robot.straight(0)
     wait(200)
-    print("Distance Settings =", robot.distance_control.limits())
-    print("Heading Settings =", robot.heading_control.limits())
+    # print("Distance Settings =", robot.distance_control.limits())
+    # print("Heading Settings =", robot.heading_control.limits())
 
 def gyro_drive_until_l(speed, angle):
     # Won't work if sensor is on black
@@ -83,5 +75,45 @@ def gyro_drive_until_r(speed, angle):
         turn_rate = kp * deviation
         robot.drive(speed, turn_rate)
         print ("Right color sensor = ",right_sensor.color())
+    robot.straight(0)
+    wait(200)
+
+def reset_on_wall():
+    timer.reset()
+    while timer.time() < 500:
+        robot.drive(-200, 0)
+    robot.straight(0)
+
+def bw_gyro_drive(speed, distance, angle):
+    robot.reset()
+    drive_distance = robot.distance()
+    kp = 5
+    new_speed = speed * -1
+    distance = distance * -1
+    while drive_distance > distance:
+        deviation = angle - gyro.angle()
+        turn_rate = kp * deviation
+        robot.drive(new_speed, turn_rate)
+        drive_distance = robot.distance()
+        wait(100)
+        #print("Gyro_Drive Angle =", gyro.angle())
+        #print("Speed =", new_speed)
+    print("Angle =", gyro.angle(), "Should be", angle)
+    robot.straight(0)
+    wait(200)
+
+def bw_gyro_drive_until_t(speed, time, angle):
+    timer.reset()
+    #robot.reset()
+    kp = 5
+    new_speed = speed * -1
+    while timer.time() < time:
+        deviation = angle - gyro.angle()
+        turn_rate = kp * deviation
+        robot.drive(new_speed, turn_rate)
+        wait(100)
+        #print("Gyro_Drive Angle =", gyro.angle())
+        #print("Speed =", new_speed)
+    print("Angle =", gyro.angle(), "Should be", angle)
     robot.straight(0)
     wait(200)
